5421920f57d2476a17b17f457ed73880a2edb28e
hortencia c <hortenciaecisneros@gmail.com>
2020-07-21 23:40:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -156,8 +156,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
- programmer_hash [:dennis_ritchie][:languages] = ["Assembly"]
-# i forgot the << (push)
+ programmer_hash [:dennis_ritchie][:languages] << "Assembly"
+ 
+# i forgot the << (push), instaed of equals "assembly" it has to be "pushed in "
  programmer_hash
 end
 
